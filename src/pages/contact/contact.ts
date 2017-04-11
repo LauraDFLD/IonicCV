faad59d42e6bc83f6b0cfa11ba8d6f897eedf67c
ludivinefafournoux <ludivine.fafournoux@hotmail.fr>
2017-04-11 15:31:59 +0200

    Ajout mail dynamique + traduction

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
-  constructor(public navCtrl: NavController) {
-
+	name: string;
+  constructor(public navCtrl: NavController, private alertController: AlertController) {
+  	
+    }
+    showName() {
+        console.log(this.name);
+        /*let alert = this.alertController.create({
+            title: 'Hello ' + this.name + '!',
+            buttons: ['OK']
+        });
+        alert.present();*/
   }
 
 }
